eb3cec0e466b1a70970c5411639fe118270e5dd2
Mahanyas Baira <123232259+mahanyasbaira@users.noreply.github.com>
2026-06-06 22:44:01 -0600

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+            hashMapA, hashMapB = {}, {}
+            #for word in range(len(strs)):
+             #   hashMapA[self.word] = 1 + hashMapA.get([self.word], 0)
+              #  hashMapB[[self.word]] = 1 + hashMapB.get([self.word], 0)
+
+                #for count in hashMapA:
+                #    if hashMapA[count] != hashMapB[count]:
+                 #       return hashMapA
+
+                #return hashMapB
+
